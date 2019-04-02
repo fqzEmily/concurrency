0f82fd0a07dc96cd122f4e30a84b248f7e43d1d5
fuqiaozhen <qiaozhenjk112@163.com>
2019-04-02 10:08:11 +0800

    准备工作：自定义一些项目中用到的注解

---
+++ src/main/java/com/fqz/concurrency/annotation/Recommend.java
@@ -0,0 +1,17 @@
+package com.fqz.concurrency.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author fuqiaozhen
+ * @create 2019/04/02
+ *用于标识推荐写法的类
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)//在编译时忽略
+public @interface Recommend {
+    String value() default "";
+}
